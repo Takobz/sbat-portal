4e374dde5fdc2df71f7d6cb68e1a64d744f05cf0
Takobz <kobelakhutso.kk@gmail.com>
2022-12-23 11:29:42 +0200

    remove unused iconfig int

--- backend/src/SBAT.Infrastructure/Data/SBATDbContext.cs
+++ backend/src/SBAT.Infrastructure/Data/SBATDbContext.cs
@@ -1,5 +1,4 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using SBAT.Core.Entities;
 
 namespace SBAT.Infrastructure.Data
@@ -7,12 +6,7 @@ namespace SBAT.Infrastructure.Data
     #pragma warning disable CS8618
     public class SBATDbContext : DbContext
     {
-        private readonly IConfiguration _configuration;
-
-        public SBATDbContext(DbContextOptions<SBATDbContext> options, IConfiguration configuration) : base(options) 
-        {
-            _configuration = configuration;
-        }
+        public SBATDbContext(DbContextOptions<SBATDbContext> options) : base(options) {}
 
         public DbSet<User> Users { get; private set; }
         public DbSet<Member> Members { get; private set; }
