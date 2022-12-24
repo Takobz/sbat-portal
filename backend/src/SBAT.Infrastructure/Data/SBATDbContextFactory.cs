887daa7e306e4bd736e05d26829375d61277f079
Takobz <kobelakhutso.kk@gmail.com>
2022-12-24 16:18:34 +0200

    add dbcontext factory for migration

---
+++ backend/src/SBAT.Infrastructure/Data/SBATDbContextFactory.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace SBAT.Infrastructure.Data
+{
+    public class DbContextFactory : IDesignTimeDbContextFactory<SBATDbContext>
+    {
+        //dotnet 5+ pass argument 0 as environment: Development or empty string for prod
+        public SBATDbContext CreateDbContext(string[] args)
+        {
+            var currentDir = Directory.GetCurrentDirectory();
+            var appsettings = (args == null || args.Length == 0) ? $"appsettings.json"
+                : $"appsettings.{args?[0]}.json";
+
+            IConfigurationRoot configurationRoot =
+                new ConfigurationBuilder()
+                    .AddJsonFile(Path.GetFullPath(Path.Combine(currentDir, appsettings)))
+                    .Build();
+
+            var connectionString = configurationRoot.GetConnectionString("sbatDatabase");
+            if(string.IsNullOrEmpty(connectionString))
+            {
+                throw new ArgumentNullException("EF Core Design Time: sbatDatabase connection string is null");
+            }
+            var optionsBuilder = new DbContextOptionsBuilder<SBATDbContext>();
+            optionsBuilder.UseSqlite(configurationRoot.GetConnectionString("sbatDatabase"));
+
+            return new SBATDbContext(optionsBuilder.Options);
+        }
+    }
+}
