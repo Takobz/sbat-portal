6821c0f1d09cff98975d25bfedea5d7b7f8fb19a
Takobz <kobelakhutso.kk@gmail.com>
2023-07-08 14:58:36 +0200

    tests: int init

---
+++ backend/Tests/SBAT.Integration.Tests/LoginControllerTests/RegisterTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Net.Http.Headers;
+using System.Net;
+
+namespace SBAT.Integration.Tests.LoginControllerTests
+{
+    public class RegisterTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+
+        public RegisterTests(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task Calling_POST_Register_With_Invalid_Data_Should_Return_BadRequest() 
+        {
+            var httpClient = _factory.CreateClient();
+            httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
+
+            var httpContent = new StringContent(string.Empty);
+            var response = await httpClient.PostAsync("/api/login/register", httpContent);
+
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+    }
+}
---
+++ backend/Tests/SBAT.Integration.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace SBAT.Integration.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
